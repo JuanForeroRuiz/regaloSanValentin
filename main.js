192ff0b46174a7ee44a0eabea61981654b925081
Juan Forero Ruiz <juancamiloforeroruiz@gmail.com>
2026-02-15 17:44:55 -0500

    regalo san valentin listo

---
+++ main.js
@@ -0,0 +1,17 @@
+alert("Hola Mi amor, espero que te guste este regalito de san valen, valen valen tin tin ja ja ja tin. Es con mucho amor y a mi mejor forma jeje");
+
+alert ("Eres perfecta:\nComo las líneas del supra\nComo la estética del skyline\nO el mazda RX-7");
+alert("Bella:\nComo el porsche 911\nEl BMW E30\nO el Nissan GT-R");
+alert("Única:\nComo el Pagani Zonda\nEl Ferrari F40\nO el McLaren Senna");
+alert("Tu mirada:\nTierna como un Miata\nAgresiva como un Mustang\nSuave como un Shelby\nPero desafiante como un Challenger");
+alert("Tu voz:\nHermosa como un motor V10\nComo el lexus LFA\nO el Renault de Fernando Alonso");
+alert("Eres valiosa:\nComo un Koenigsegg Agera\nComo un Bugatti Veyron\nO el Ferrari Enzo");
+alert("ERES TODO LO QUE QUIERO:\nComo una MT-09\nComo un motor tetracilíndrico\nO una GSX-S 750\n\nE incluso, hasta la carretera de mi camino");
+
+alert("Se que hemos pasado por altos, bajos, duras, suaves, pero quiero estar contigo, quiero confiarte mi vida, mis deseos, mis secretos, y cumplir mis sueños contigo. Quiero ser el que elijas siempre al despertar, al dormir, al estudiar, al correr, al existir.");
+
+alert("Quiero seguir contigo millones de meses, días, segundos, minutos, horas más. Quiero ser tu pañuelo de lagrimas cuando quieras llorar, tu psicologo cuando quieras desahogar, tu mejor amiga cuando quieras hablar y echar chisme, tu instructor cuando no entiendas algo.....");
+
+alert("En fin. Quiero que al final del día a pesar de todo nos sigamos eligiendo el uno al otro y no dudemos nunca de estar. Y si llega a pasar que nos tengamos que separar quiero que lo hagamos por el amor tan grande que nos tenemos para no hacernos daño");
+alert("Feliz día de San Valentín mi amor. Te amo millones mi niña");
+
